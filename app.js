c93846bf659ecc0667f1072476767513d226086a
Khattech <shivamkhattri123@gmail.com>
2020-08-07 19:24:12 +0530

    Updated

---
+++ app.js
@@ -0,0 +1,17 @@
+const request = require("request");
+
+request('http://wwwle.com', (error, response,body)=>{
+
+	if(error)
+	{
+		console.log(error);
+	}
+	else
+	{
+       if(response.statusCode == 200)
+       {
+       	console.log(body);
+       }
+	}
+
+});
